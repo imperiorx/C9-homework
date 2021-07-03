e0264da6765538532ea874f71e00e64984680bc4
imperiorx <86550040+imperiorx@users.noreply.github.com>
2021-07-03 16:15:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(DOWN_ARROW))
+{
+  background("blue");
+}
+if(keyIsDown(UP_ARROW))
+{
+  background("yellow");
+}
+if(keyIsDown(LEFT_ARROW))
+{
+  background("purple");
+}
+if(keyIsDown(RIGHT_ARROW))
+{
+  background("red");
+}
+}
+
+
+
+
